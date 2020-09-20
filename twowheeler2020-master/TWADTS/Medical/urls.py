e9f103a1cf99c32aa798b8a216a635ae8e52bc18
Shubham Kedare <kedareshubham11@gmail.com>
2020-09-20 21:10:42 +0530

    clean

--- twowheeler2020-master/TWADTS/Medical/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'Medical'
-urlpatterns = [
-    path('add_medical_detail', views.add_medical_detail, name='add_medical_detail'),
-    path('medical_history', views.medical_history, name='medical_history'),
-]
--- twowheeler2020-master/TWADTS/Users/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import SignUp,Personal_Info,Bike_Info
-admin.site.register(SignUp)
-admin.site.register(Personal_Info)
-admin.site.register(Bike_Info)
-# Register your models here.
